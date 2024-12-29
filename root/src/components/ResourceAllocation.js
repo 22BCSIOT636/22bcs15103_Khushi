7f2b0176170114fcf538bfa83828befe92a9cfa0
Khushi Vishwakarma <22bcs15103@cuchd.in>
2024-12-29 12:10:05 +0530

    Create ResourceAllocation.js

---
+++ root/src/components/ResourceAllocation.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ResourceAllocation() {
+  return (
+    <div>
+      <h2>Resource Allocation</h2>
+      <p>Optimize services during peak times.</p>
+    </div>
+  );
+}
+
+export default ResourceAllocation;
