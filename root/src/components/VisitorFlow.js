2f1878922c4d1cee8a9bcbf64fb3fa75ce98257e
Khushi Vishwakarma <22bcs15103@cuchd.in>
2024-12-29 12:09:27 +0530

    Create VisitorFlow.js

---
+++ root/src/components/VisitorFlow.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function VisitorFlow() {
+  return (
+    <div>
+      <h2>Visitor Flow Management</h2>
+      <p>Details about visitor flow to reduce congestion.</p>
+    </div>
+  );
+}
+
+export default VisitorFlow;
