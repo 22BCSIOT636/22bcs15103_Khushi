2e2a9661d18b4375e64156f50082cfd5418690ab
Khushi Vishwakarma <22bcs15103@cuchd.in>
2024-12-29 12:08:53 +0530

    Create PredictionChart.js

---
+++ root/src/components/PredictionChart.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function PredictionChart() {
+  return (
+    <div>
+      <h2>Footfall Predictions</h2>
+      <p>Visualize forecasted visitor density here.</p>
+      {/* Add chart visualization later */}
+    </div>
+  );
+}
+
+export default PredictionChart;
